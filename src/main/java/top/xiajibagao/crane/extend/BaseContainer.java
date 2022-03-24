60fb132caafa1d54e1d0bb4bedb2708f0495ff2d
huangchengxing <huangchengxing@nova.net.cn>
2022-03-24 20:57:32 +0800

    refactor: 模板基类添加数据源泛型;

--- src/main/java/top/xiajibagao/crane/extend/BaseContainer.java
+++ src/main/java/top/xiajibagao/crane/extend/BaseContainer.java
@@ -1,24 +1,27 @@
 package top.xiajibagao.crane.extend;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import top.xiajibagao.crane.container.Container;
 import top.xiajibagao.crane.helper.CollUtils;
 import top.xiajibagao.crane.helper.ObjectUtils;
-import top.xiajibagao.crane.helper.PairEntry;
 import top.xiajibagao.crane.parse.interfaces.AssembleOperation;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * 容器基类，用于提供针对“使用namespace和id获取数据源并操作”的模板代码
  *
- * @param <K> id字段类型
+ * @param <T> source类型
+ * @param <K> key字段类型
  * @author huangchengxing
  * @date 2022/03/23 21:07
  */
 @Slf4j
-public abstract class BaseContainer<K> implements Container {
+public abstract class BaseContainer<K, T> implements Container {
 
     @Override
     public void process(List<Object> targets, List<AssembleOperation> operations) {
@@ -26,14 +29,14 @@ public abstract class BaseContainer<K> implements Container {
             return;
         }
         // 获取key值与命名空间
-        List<PairEntry<K, String>> keyEnters = getKeyEntersFromTargets(targets, operations);
-        if (CollUtils.isEmpty(keyEnters)) {
+        MultiValueMap<String, K> namespacesAndKeys = getNamespaceAndKeyFromTargets(targets, operations);
+        if (CollUtils.isEmpty(namespacesAndKeys)) {
             return;
         }
         // 根据key值获取数据源
-        Map<String, Map<K, ?>> sources = ObjectUtils.trySupply(
-            () -> getSources(keyEnters),
-            e -> log.warn("容器[{}]获取数据源获取失败，key[{}]，错误信息：{}", this.getClass(), keyEnters, e.getMessage())
+        Map<String, Map<K, T>> sources = ObjectUtils.trySupply(
+            () -> getSources(namespacesAndKeys),
+            e -> log.warn("容器[{}]获取数据源获取失败，数据[{}]，错误信息：{}", this.getClass(), namespacesAndKeys, e.getMessage())
         );
         if (CollUtils.isEmpty(sources)) {
             return;
@@ -60,50 +63,51 @@ public abstract class BaseContainer<K> implements Container {
     /**
      * 根据数据源id与对应的namespace获取数据集
      *
-     * @param keyEnters id-namespace集合
-     * @return java.util.Map<java.lang.String,java.util.Map<K,?>> 命名空间-key值-数据源对象双重集合
+     * @param namespaceAndKeys namespace-key字段集合
+     * @return java.util.Map<java.lang.String,java.util.Map<K, T>> namespace-key-数据源对象集合
      * @author huangchengxing
      * @date 2022/3/21 12:17
      */
-    protected abstract Map<String, Map<K, ?>> getSources(List<PairEntry<K, String>> keyEnters);
+    protected abstract Map<String, Map<K, T>> getSources(MultiValueMap<String, K> namespaceAndKeys);
 
     /**
-     * 从对象中获取所需要的数据源id
+     * 将对象集合转为所需要的namespace与key集合
      *
      * @param target 对象
      * @param operations 操作配置
-     * @return java.util.Set<K>
+     * @return org.springframework.util.MultiValueMap<java.lang.String,K>
      * @author huangchengxing
      * @date 2022/3/21 12:17
      */
-    protected List<PairEntry<K, String>> getKeyEntersFromTarget(Object target, List<AssembleOperation> operations) {
-        List<PairEntry<K, String>> keyEnters = new ArrayList<>();
+    protected MultiValueMap<String, K> getKeyAndNamespaceFromTarget(Object target, List<AssembleOperation> operations) {
+        MultiValueMap<String, K> results = new LinkedMultiValueMap<>();
         for (AssembleOperation operation : operations) {
             Object key = operation.getAssembler().getKey(target, operation);
             K actualKey = parseKey(key);
             if (Objects.nonNull(actualKey)) {
-                keyEnters.add(new PairEntry<>(actualKey, operation.getNamespace()));
+                results.add(operation.getNamespace(), actualKey);
             }
         }
-        return keyEnters;
+        return results;
     }
 
     /**
-     * 将对象集合转为所需要的数据源id
+     * 将对象集合转为所需要的namespace与key集合
      *
      * @param targets 对象集合
      * @param operations 操作配置
-     * @return java.util.Set<K>
+     * @return org.springframework.util.MultiValueMap<java.lang.String,K>
      * @author huangchengxing
      * @date 2022/3/21 12:17
      */
-    protected List<PairEntry<K, String>> getKeyEntersFromTargets(List<Object> targets, List<AssembleOperation> operations) {
-        return targets.stream()
+    protected MultiValueMap<String, K> getNamespaceAndKeyFromTargets(List<Object> targets, List<AssembleOperation> operations) {
+        MultiValueMap<String, K> results = new LinkedMultiValueMap<>();
+        targets.stream()
             .filter(Objects::nonNull)
-            .map(t -> getKeyEntersFromTarget(t, operations))
+            .map(t -> getKeyAndNamespaceFromTarget(t, operations))
             .filter(CollUtils::isNotEmpty)
-            .flatMap(Collection::stream)
-            .collect(Collectors.toList());
+            .forEach(results::addAll);
+        return results;
     }
 
     /**
