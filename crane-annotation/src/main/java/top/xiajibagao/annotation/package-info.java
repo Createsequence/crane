1017727372d7e72d30592d31a4b279fed943dd11
huangchengxing <huangchengxing@nova.net.cn>
2022-04-18 06:14:28 +0800

    docs: 添加package-info;

---
+++ crane-annotation/src/main/java/top/xiajibagao/annotation/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * crane注解包
+ *
+ * @author huangchengxing
+ * @date 2022/04/18 6:02
+ */
+package top.xiajibagao.annotation;
---
+++ crane-spring-boot-starter/src/main/java/top/xiajibagao/crane/starter/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * springboot启动器包
+ * <p>提供crane在springboot环境下的各项自动配置功能
+ *
+ * @author huangchengxing
+ * @date 2022/04/18 6:13
+ */
+package top.xiajibagao.crane.starter;
