5d12cb37abfc8872fe5a8b347822896b24ee68e4
huangchengxing <huangchengxing@nova.net.cn>
2022-06-01 13:17:29 +0800

    test: 添加缓存解析器包装类的测试用例;

---
+++ crane-spring-boot-starter/src/main/test/java/top/xiajiabagao/crane/starter/core/cache/CacheConfigurationParserWrapperTest.java
@@ -0,0 +1,52 @@
+package top.xiajiabagao.crane.starter.core.cache;
+
+import lombok.Data;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import top.xiajiabagao.crane.starter.core.ExampleConfig;
+import top.xiajibagao.crane.core.cache.CacheConfigurationParserWrapper;
+import top.xiajibagao.crane.core.cache.ConfigurationCache;
+import top.xiajibagao.crane.core.cache.OperationConfigurationCache;
+import top.xiajibagao.crane.core.parser.FieldAnnotationConfigurationParser;
+import top.xiajibagao.crane.core.parser.interfaces.GlobalConfiguration;
+import top.xiajibagao.crane.core.parser.interfaces.OperateConfigurationParser;
+import top.xiajibagao.crane.core.parser.interfaces.OperationConfiguration;
+
+/**
+ * @author huangchengxing
+ * @date 2022/06/01 11:11
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ExampleConfig.class)
+public class CacheConfigurationParserWrapperTest {
+
+    @Autowired
+    GlobalConfiguration globalConfiguration;
+    @Autowired
+    BeanFactory beanFactory;
+
+    @Test
+    public void testCacheConfigurationParserWrapper() {
+        ConfigurationCache configurationCache = new OperationConfigurationCache();
+        OperateConfigurationParser fieldAnnotationConfigurationParser = new FieldAnnotationConfigurationParser(globalConfiguration, beanFactory);
+        String cacheName = fieldAnnotationConfigurationParser.getClass().getName();
+        Assertions.assertNull(configurationCache.getCachedConfiguration(cacheName, Example.class));
+
+        OperateConfigurationParser wrapperParser = new CacheConfigurationParserWrapper(configurationCache, fieldAnnotationConfigurationParser);
+        OperationConfiguration configuration = wrapperParser.parse(Example.class);
+        Assertions.assertEquals(configuration, configurationCache.getCachedConfiguration(cacheName, Example.class));
+        OperationConfiguration cachedConfiguration = wrapperParser.parse(Example.class);
+        Assertions.assertEquals(configuration, cachedConfiguration);
+    }
+
+    @Data
+    private static class Example {
+
+    }
+
+}
