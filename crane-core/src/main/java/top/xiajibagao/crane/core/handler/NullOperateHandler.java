34921c6ad7afb29379a5be770c28168955009a5a
huangchengxing <841396397@qq.com>
2022-06-07 17:06:02 +0800

    feat: 添加null值操作处理器;

---
+++ crane-core/src/main/java/top/xiajibagao/crane/core/handler/NullOperateHandler.java
@@ -0,0 +1,48 @@
+package top.xiajibagao.crane.core.handler;
+
+import top.xiajibagao.crane.core.handler.interfaces.OperateHandler;
+import top.xiajibagao.crane.core.helper.Orderly;
+import top.xiajibagao.crane.core.parser.interfaces.AssembleOperation;
+import top.xiajibagao.crane.core.parser.interfaces.Operation;
+import top.xiajibagao.crane.core.parser.interfaces.PropertyMapping;
+
+import javax.annotation.Nullable;
+import java.util.Objects;
+
+/**
+ * 用于处理null值的节点，一般当具有最高的优先级，当待读取的数据源或待写入的待处理对象为null时，
+ * 优先使用该节点处理，避免后续节点被无意义的反复调用
+ *
+ * @author huangchengxing
+ * @date 2022/06/07 16:49
+ * @since 0.5.5
+ */
+public class NullOperateHandler implements OperateHandler {
+
+    @Override
+    public int getOrder() {
+        return Orderly.HIGHEST_PRECEDENCE;
+    }
+
+    @Override
+    public boolean sourceCanRead(@Nullable Object source, PropertyMapping property, Operation operation) {
+        return Objects.isNull(source);
+    }
+
+    @Override
+    public boolean targetCanWrite(@Nullable Object sourceData, @Nullable Object target, PropertyMapping property, AssembleOperation operation) {
+        return Objects.isNull(target);
+    }
+
+    @Override
+    public Object readFromSource(@Nullable Object source, PropertyMapping property, Operation operation) {
+        // 直接返回null
+        return null;
+    }
+
+    @Override
+    public void writeToTarget(@Nullable Object sourceData, @Nullable Object target, PropertyMapping property, AssembleOperation operation) {
+        // 不做任何处理
+    }
+
+}
