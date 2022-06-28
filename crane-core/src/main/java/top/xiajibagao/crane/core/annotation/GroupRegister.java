f811a2efdf9cc150f99894044a82f8c50ec1ce18
huangchengxing <841396397@qq.com>
2022-06-28 15:49:43 +0800

    refactor: 修复注释错误;

--- crane-core/src/main/java/top/xiajibagao/crane/core/annotation/GroupRegister.java
+++ crane-core/src/main/java/top/xiajibagao/crane/core/annotation/GroupRegister.java
@@ -1,18 +1,17 @@
 package top.xiajibagao.crane.core.annotation;
 
-import top.xiajibagao.crane.core.operator.interfaces.*;
+import top.xiajibagao.crane.core.operator.interfaces.GroupRegistrable;
+import top.xiajibagao.crane.core.operator.interfaces.Operator;
 
 import java.lang.annotation.*;
 
 /**
- * <p>用于标记{@link SourceReader}，{@link SourceReadInterceptor}，{@link TargetWriter}或{@link SourceReadInterceptor}，
- * 表示该组件允许被注册到哪些{@link Operator}中
+ * <p>用于标记{@link GroupRegistrable}接口的实现类，表示该组件允许被注册到哪些分组中
  *
  * @author huangchengxing
  * @date 2022/06/27 15:03
- * @see GroupRegistrable#OPERATE_GROUP_JAVA_BEAN
- * @see GroupRegistrable#OPERATE_GROUP_JSON_BEAN
- * @see 0.5.8
+ * @since 0.5.8
+ * @see GroupRegistrable
  */
 @MateAnnotation
 @Target(ElementType.TYPE)
